09c2f3adfa403a49b72241ee050a661864032dbc
kirangautam45 <kirangautam225@gmail.com>
2024-09-13 14:44:46 +0545

    Todo Application

--- src/component/todo/AddTodo.tsx
+++ src/component/todo/AddTodo.tsx
@@ -4,10 +4,14 @@ import { todoListState } from '../recoil/atoms'
 
 const AddTodo = () => {
   const [inputValue, setInputValue] = useState('')
+  const [error, setError] = useState('')
   const setTodoList = useSetRecoilState(todoListState)
 
   const addItem = () => {
-    if (inputValue.trim() === '') return
+    if (inputValue.trim() === '') {
+      setError("Todo list can't be empty")
+      return
+    }
 
     setTodoList((oldTodoList) => [
       ...oldTodoList,
@@ -18,7 +22,16 @@ const AddTodo = () => {
       },
     ])
     setInputValue('')
+    setError('')
   }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+    if (error) {
+      setError('')
+    }
+  }
+
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       addItem()
@@ -26,28 +39,32 @@ const AddTodo = () => {
   }
 
   return (
-    <div className='flex items-center lg:w-1/2 w-full m-2 mt-4 p-2 border-y'>
-      <div className='size-6 mr-4 border-2 border-purple-600 bg-purple-600 rounded-full flex items-center justify-center'>
-        <span className='flex items-center justify-center text-3xl font-bold mb-1 text-white'>
-          +
-        </span>
-      </div>
+    <>
+      <div className='flex flex-row items-center lg:w-1/2 w-full m-2 mt-4 p-2 border-y'>
+        <div className='size-6 mr-4 border-2 border-purple-600 bg-purple-600 rounded-full flex items-center justify-center'>
+          <span className='flex items-center justify-center text-3xl font-bold mb-1 text-white'>
+            +
+          </span>
+        </div>
 
-      <input
-        type='text'
-        value={inputValue}
-        onKeyDown={handleKeyPress}
-        onChange={(e) => setInputValue(e.target.value)}
-        className=' p-2 lg:w-full w-10/12 focus:outline-none text-[16px] font-semibold'
-        placeholder='memorize the dictionary'
-      />
-      <button
-        className='bg-purple-600 w-[140px] text-white px-4 py-2 rounded font-semibold text-[14px]'
-        onClick={addItem}
-      >
-        Add Items
-      </button>
-    </div>
+        <input
+          type='text'
+          value={inputValue}
+          onKeyDown={handleKeyPress}
+          onChange={handleChange}
+          className=' p-2 lg:w-full w-10/12 focus:outline-none text-[16px] font-semibold'
+          placeholder='memorize the dictionary'
+        />
+
+        <button
+          className='bg-purple-600 w-[140px] text-white px-4 py-2 rounded font-semibold text-[14px]'
+          onClick={addItem}
+        >
+          Add Items
+        </button>
+      </div>
+      {error && <div className='text-red-600 text-sm mt-2 '>{error}</div>}
+    </>
   )
 }
 
